a5a8ef88b369beb644c336f71bcef12a38b024ab
chawn <chawn.chen@gmail.com>
2023-03-22 20:13:45 +0800

    更新日志

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+src=~/gitbook-pre
+dst=~/sth
+cd $src
+gitbook build ./ ~/buildbook/
+
+cd $dst
+git checkout gh-pages
+if [[ $? -eq 0 ]];then
+  /bin/cp -rf ~/gitbook-pre/*.md  .
+  /bin/cp -rf ~/buildbook/*  .
+  git add .
+  git commit -m "更新日志"
+  git push origin
+fi
